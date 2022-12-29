4bb09a745c624f4a6c7978e89d86fac175836a99
Сергей Андреев <andr140704@mail.ru>
2022-12-29 21:49:56 +0300

    done

--- lab8/Task8_3/ArrayExtensions.cs
+++ lab8/Task8_3/ArrayExtensions.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
-namespace lab8.Task8_3
+namespace Anagrams
 {
-    internal class ArrayExtensions
+    public static class ArrayExtensions
     {
+        public static void Sort(this Array array, IComparer comparer)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    object element1 = array.GetValue(j - 1);
+                    object element2 = array.GetValue(j);
+                    if (comparer.Compare(element1, element2) > 0)
+                    {
+                        object temporary = array.GetValue(j);
+                        array.SetValue(array.GetValue(j - 1), j);
+                        array.SetValue(temporary, j - 1);
+                    }
+
+                }
+
+            }
+
+        }
+
     }
+
 }
